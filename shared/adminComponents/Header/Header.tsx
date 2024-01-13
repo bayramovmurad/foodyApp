a7fadda50821c78b6beb5bafa06f026e8b782d72
MuradBayramovDeveloper <125645966+MuradBayramov1@users.noreply.github.com>
2024-01-13 21:28:36 +0400

    complete adminHeader

--- shared/adminComponents/Header/Header.tsx
+++ shared/adminComponents/Header/Header.tsx
@@ -1,6 +1,20 @@
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="bg-[#27283c] flex items-center justify-between mx-[19px] h-16 px-6 rounded-b-xl">
+      <div className="w-[92px] h-8 text-center"><span className="text-neutral-100 text-[28px] font-extrabold font-['Mukta'] leading-normal tracking-wide">Foody</span><span className="text-yellow-500 text-[28px] font-extrabold font-['Mukta'] leading-normal tracking-wide">.</span></div>
+
+      <div className="flex items-center gap-x-[30px]">
+        <div className="w-[107px] flex items-center text-white h-7 bg-fuchsia-700 rounded-[14px] shadow border-2 border-fuchsia-700">
+        <img src="/adminImg/Header/Plus.svg" alt="" />
+        <div className="font-semibold text-[10px]">ADD PRODUCT</div>
+      </div>
+      <div className="flex gap-x-2 items-center">
+        <img className="rounded-full" src="/adminImg/Header/User.svg" alt="" />
+          <p className="text-white font-semibold">Admin</p>
+      </div>
+      </div>
+      
+    </div>
   )
 }
 export default Header
--- shared/adminComponents/SideBar/SideBar.tsx
+++ shared/adminComponents/SideBar/SideBar.tsx
@@ -21,7 +21,7 @@ const SideBar: FC = () => {
     <div className="w-64 h-[474px] pl-6 pt-[30px] relative bg-fuchsia-500 rounded-[14px] flex flex-col gap-y-2">
       {["Dashboard", "Products", "Restaurants", "Category", "Orders", "Offer", "Logout"].map((item) => (
         <div key={item} className="w-64 h-10 relative flex items-center" onClick={() => handleSelect(item)}>
-          <div className={selectedItem === item ? "w-[216px] h-10 top-0 absolute opacity-10 bg-white rounded" : ""} />
+          <div className={selectedItem === item ? "w-[216px] h-10 top-0 absolute opacity-10 bg-[#fff] rounded" : ""} />
           <div className="w-6 h-6 absolute" />
           <div className="w-[143px] h-[18px] pl-[14px] absolute">
             <span className="flex gap-x-6 text-white">
