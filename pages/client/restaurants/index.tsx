91b8a0d9a8d7af8d5d28842f58f00d4926366107
Ramal Abbassov <ramalabbasov044@gmail.com>
2024-01-14 23:28:37 +0400

    add

--- pages/client/restaurants/index.tsx
+++ pages/client/restaurants/index.tsx
@@ -1,20 +1,114 @@
-import type { NextPage } from "next";
-
+import RestaurantCard from '../../../shared/components/RestaurantCard/index'
 import Header from "../../../shared/components/Header/index";
 import Footer from '../../../shared/components/Footer/index'
 
-const Restaurants: NextPage = () => {
+
+import Image from "next/image";
+
+const Restaurants = () => {
   return (
     <div className="bg-white">
         <div className="p-[30px]">
           <Header
-              isLogin={false}
-              isBasket={false}
-              isAvatar={false}
+              isLogin={true}
+              isBasket={true}
+              isAvatar={true}
               isName={false}
-              isBottom={true}
+              isBottom={false}
           />
         </div>
+
+        <main className="p-[30px] flex justify-center">
+            <div className='max-w-[1440px] w-full flex gap-[40px]'>
+              <div className="left max-w-[251px] w-full py-[40px] pl-[25px] pr-[25px] bg-[#F3F4F6] flex flex-col gap-[25px]">
+                  <div className="filterItem flex gap-[17px] cursor-pointer">
+                      <Image
+                          src="/client/minifood/pizza.svg"
+                          alt="pizza"
+                          width={25}
+                          height={28}
+                      />
+                      <p className="text-[#333] text-[20px] font-semibold">
+                        Chinese
+                      </p>
+                  </div>
+
+                  <div className="filterItem flex gap-[17px] cursor-pointer">
+                      <Image
+                          src="/client/minifood/pizza.svg"
+                          alt="pizza"
+                          width={25}
+                          height={28}
+                      />
+                      <p className="text-[#333] text-[20px] font-semibold">
+                        Chinese
+                      </p>
+                  </div>
+
+                  <div className="filterItem flex gap-[17px] cursor-pointer">
+                      <Image
+                          src="/client/minifood/pizza.svg"
+                          alt="pizza"
+                          width={25}
+                          height={28}
+                      />
+                      <p className="text-[#333] text-[20px] font-semibold">
+                        Chinese
+                      </p>
+                  </div>
+                  
+                  <div className="filterItem flex gap-[17px] cursor-pointer">
+                      <Image
+                          src="/client/minifood/pizza.svg"
+                          alt="pizza"
+                          width={25}
+                          height={28}
+                      />
+                      <p className="text-[#333] text-[20px] font-semibold">
+                        Chinese
+                      </p>
+                  </div>
+              </div>
+
+              <div className="cardBody grid grid-cols-4 gap-[40px]">
+                  <RestaurantCard
+                      path={"/client/restaurant/food.svg"}
+                      name={"Coffee Mania"}
+                      description={"chinese, sea-food, thai, lebanese, caribbean"}
+                      price={5}
+                      time={"09 Min"}
+                  />  
+                  <RestaurantCard
+                      path={"/client/restaurant/food.svg"}
+                      name={"Coffee Mania"}
+                      description={"chinese, sea-food, thai, lebanese, caribbean"}
+                      price={5}
+                      time={"09 Min"}
+                  />  
+                  <RestaurantCard
+                      path={"/client/restaurant/food.svg"}
+                      name={"Coffee Mania"}
+                      description={"chinese, sea-food, thai, lebanese, caribbean"}
+                      price={5}
+                      time={"09 Min"}
+                  />  
+                  <RestaurantCard
+                      path={"/client/restaurant/food.svg"}
+                      name={"Coffee Mania"}
+                      description={"chinese, sea-food, thai, lebanese, caribbean"}
+                      price={5}
+                      time={"09 Min"}
+                  />  
+                  <RestaurantCard
+                      path={"/client/restaurant/food.svg"}
+                      name={"Coffee Mania"}
+                      description={"chinese, sea-food, thai, lebanese, caribbean"}
+                      price={5}
+                      time={"09 Min"}
+                  />  
+              </div>
+            </div>
+        </main>
        
 
         <Footer
