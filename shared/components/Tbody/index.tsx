30712880c082f726880d1bc528f2578f79d5d176
Ramal Abbassov <ramalabbasov044@gmail.com>
2024-01-18 23:00:32 +0400

    add

--- shared/components/Tbody/index.tsx
+++ shared/components/Tbody/index.tsx
@@ -1,48 +1,127 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
+import Modal from '../Modal';
 
 interface TBodyProps {
-  data: { 
+  item: { 
     id: string; 
     time: string; 
     address: string; 
     amount: number; 
     paymentMethod: string; 
     contact: string 
-  }[];
+  };
+  deleteOrder: (id: string) => void;
 }
 
-const TBody: FC<TBodyProps> = ({ data }) => {
-  return (
-    <tbody className='bg-white h-[1000px]'>
-      {data.map((item) => (
-        <tr className=' border-t border-b border-[#DFE2E9]'>
-            <td className='py-[14px] px-[12px]'>
-              {item.id}
-            </td>
+const TBody: FC<TBodyProps> = ({ item , deleteOrder }) => {
+      const [isMenu,setIsMenu] = useState<boolean>(false)
+      const [deleteModal,setDeleteModal] = useState<boolean>(false)
 
-            <td className='py-[14px] px-[12px]'>
-              {item.time}
-            </td>
+      const showModal = (id: string): void => {
+          setIsMenu(!isMenu)
+      }
 
-            <td className='py-[14px] px-[12px]'>
-              {item.address}
-            </td>
+      const deleteItem = (id: string): void => {
+          setDeleteModal(!deleteModal)
+      }
 
-            <td className='py-[14px] px-[12px]'>
-              {item.amount}
-            </td>
+      const response = (boolParam: boolean) => {
+          if(boolParam){
+              deleteOrder(item.id)
+              setIsMenu(!isMenu)
+              setDeleteModal(!deleteModal)
+          }else{
+              setIsMenu(!isMenu)
+              setDeleteModal(!deleteModal)
+          }
+      }
+  
+      return (
+        <>
+            <tr className='relative max-h-[50px] h-[50px] border-t border-b border-[#DFE2E9]'>
+              <td className='px-[12px] h-[50px]'>
+                  <p className='font-normal text-[14px] border border-[#DFE2E9] text-[#454D59] py-[2px] px-[12px] rounded-lg text-center'>
+                    {
+                      item.id
+                    }
+                  </p>
+              </td>
+              <td className='px-[12px]'>
+                    <p className='text-center text-[14px] text-[#00072B] font-normal'>
+                      {
+                        item.time
+                      }
+                    </p>
+              </td>
+              <td className='px-[12px] text-center flex items-center justify-center h-full'>
+                  <p className='max-w-[183px] text-[#00072B] text-[14px] font-normal text-center'>
+                    {
+                      item.address
+                    }
+                  </p>
+              </td>
+              <td className='px-[12px] text-center'>
+                    <p className='text-center text-[#00072B] font-normal text-[14px]'>
+                        {
+                          item.amount
+                        }
+                    </p>
+              </td>
+              <td className='px-[12px] text-center'>
+                    <p className='text-center text-[#00072B] font-normal text-[14px]'>
+                        {
+                          item.paymentMethod
+                        }
+                    </p>
+              </td>
+              <td className='px-[12px] text-center'>
+                    <p className='text-center text-[#00072B] font-normal text-[14px]'>
+                        {
+                          item.contact
+                        }
+                    </p>
+              </td>
+              
+              <td className='cursor-pointer' onClick={() => showModal(item.id)}>
+                <svg width="4" height="16" viewBox="0 0 4 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <rect width="4" height="4" rx="2" fill="#828282"/>
+                    <rect y="6" width="4" height="4" rx="2" fill="#828282"/>
+                    <rect y="12" width="4" height="4" rx="2" fill="#828282"/>
+                </svg>
+              </td>
 
-            <td className='py-[14px] px-[12px]'>
-              {item.paymentMethod}
-            </td>
+              {
+                  isMenu ? (
+                    <div style={{boxShadow:"0px 3px 8px -2px rgba(0, 0, 0, 0.20)"}} className='bg-white flex flex-col gap-2 z-10 top-[30px] right-[10px] absolute py-[5px] px-[13px]'>
+                        <p onClick={() => showItem()} className='text-[#6FCF97] text-[14px] font-normal cursor-pointer'>
+                            show
+                        </p>
+                        <p onClick={() => deleteItem(item.id)} className='text-[#EB5757] text-[14px] font-normal cursor-pointer'>
+                            delete
+                        </p>
+                    </div>
+                  ) : (
+                    <></>
+                  )
+              }
 
-            <td className='py-[14px] px-[12px]'>
-              {item.contact}
-            </td>
-        </tr>
-      ))}
-    </tbody>
-  );
+              {
+                deleteModal ? (
+                  <Modal
+                      title={"Are you sure it’s deleted ?"}
+                      description={"Attention! If you delete this order, it will not come back..."}
+                      response={response}
+                      modalResponseTitle={"delete"}
+                  />
+                ) : (
+                  <>
+
+                  </>
+                )
+              }
+            </tr>
+        </>
+      );
 }
 
 export default TBody;
--- shared/components/Thead/index.tsx
+++ shared/components/Thead/index.tsx
@@ -7,7 +7,7 @@ interface TheadProps {
 const Thead: FC<TheadProps> = ({ items }) => {
   return (
       <thead className='bg-white w-full'>
-            <tr className='h-[54px] pt-6 w-full'>
+            <tr className='pt-6 w-full h-full'>
                 {
                     items.map((item: string) => (
                         <th className='h-[30px] px-[12px] text-[#00072B] text-[14px] font-semibold'>
