7ddf788311ff1cb6213a4442318ef0b96ef05268
Ramal Abbassov <ramalabbasov044@gmail.com>
2024-01-29 20:30:25 +0400

    update config files

--- config/i18n/i18n.js
+++ config/i18n/i18n.js
@@ -1,46 +1,57 @@
 import i18n from "i18next";
 import {initReactI18next} from 'react-i18next';
-
 const resources = {
 	en: {
 		translation: {
-			"home": "Home",
-            "restaurants": "Restaurants",
-            "about": "About us",
-            "howitworks": "How it works",
-            "programming":  "Programming",
-            "child":  "Books for children",
-			"pyshc": "Psychology",
-			"pop": "Popular",
-            "faq":  "Faqs",
-			"explore": "Explore now",
-			"register": "Register",
-			'footerDescription': "Lorem ipsum is placeholder text commonly used in the graphic, ",
-			"order": "Order Now",
-			"search": "Search",
-			"headerTitle": "Our Food site makes it easy to find local food",
-			"headerDescription": "Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups."
+			//! Login
+				"loginHeadTitle": "Welcome Admin",
+				"loginUserName": "Username",
+				"loginPassword": "Password",
+				"loginSaveTitle": "Sign In",
+
+				"home": "Home",
+				"restaurants": "Restaurants",
+				"about": "About us",
+				"howitworks": "How it works",
+				"programming":  "Programming",
+				"child":  "Books for children",
+				"pyshc": "Psychology",
+				"pop": "Popular",
+				"faq":  "Faqs",
+				"explore": "Explore now",
+				"register": "Register",
+				'footerDescription': "Lorem ipsum is placeholder text commonly used in the graphic, ",
+				"order": "Order Now",
+				"search": "Search",
+				"headerTitle": "Our Food site makes it easy to find local food",
+				"headerDescription": "Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups."
 		}
 	},
 	az: {
 		translation: {
-			"home": "Ana Səhifə",
-            "restaurants": "Restoranlar",
-            "about": "Haqqımızda",
-            "howitworks": "Necə işləyir",
-			"programming":  "Proqramlaşdırma",
-            "child":  "Uşaqlar üçün kitablar",
-			"pyshc": "Psixologiya",
-			"pop": "Məşhur",
-			"explore": "İndi araşdırın",
-            "faq":  "Tez-tez verilən suallar",
-			'signup': "Qeydiyyat",
-			"order": "İndi sifariş edin",
-			'footerDescription': "Lorem ipsum, qrafikdə çox istifadə olunan yer tutucu mətndir",
-			"register": "Qeydiyyat",
-			"search": "Axtarış",
-			"headerTitle": "Qida saytımız yerli yeməkləri tapmağı asanlaşdırır",
-			"headerDescription": "Lorem ipsum qrafika, çap və nəşriyyat sənayelərində tərtibat və vizual maketləri önizləmək üçün geniş istifadə olunan yer tutucu mətndir."
+			//! Login
+				"loginHeadTitle": "Adminə Xoş gəldiniz",
+				"loginUserName": "İstifadəçi adı",
+				"loginPassword": "Parol",
+				"loginSaveTitle": "Daxil olun",
+
+				"home": "Ana Səhifə",
+				"restaurants": "Restoranlar",
+				"about": "Haqqımızda",
+				"howitworks": "Necə işləyir",
+				"programming":  "Proqramlaşdırma",
+				"child":  "Uşaqlar üçün kitablar",
+				"pyshc": "Psixologiya",
+				"pop": "Məşhur",
+				"explore": "İndi araşdırın",
+				"faq":  "Tez-tez verilən suallar",
+				'signup': "Qeydiyyat",
+				"order": "İndi sifariş edin",
+				'footerDescription': "Lorem ipsum, qrafikdə çox istifadə olunan yer tutucu mətndir",
+				"register": "Qeydiyyat",
+				"search": "Axtarış",
+				"headerTitle": "Qida saytımız yerli yeməkləri tapmağı asanlaşdırır",
+				"headerDescription": "Lorem ipsum qrafika, çap və nəşriyyat sənayelərində tərtibat və vizual maketləri önizləmək üçün geniş istifadə olunan yer tutucu mətndir."
 		}
 	}
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import '../config/i18n/i18n'
+import 'react-toastify/dist/ReactToastify.css';
+
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
