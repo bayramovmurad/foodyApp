97b95a6c004b2dc1a6a143e2a9806f10ebeef222
Ramal Abbassov <ramalabbasov044@gmail.com>
2024-02-08 22:19:21 +0400

    over navbar

--- pages/client/restaurants/index.tsx
+++ pages/client/restaurants/index.tsx
@@ -4,118 +4,116 @@ import Footer from '../../../shared/components/Footer/index'
 
 
 import Image from "next/image";
-
+import { useEffect, useState } from 'react';
+import { getProducts } from '../../../services/index'
 const Restaurants = () => {
-  return (
-    <div className="bg-white">
-        <div className="p-[30px]">
-          <Header
-              isLogin={true}
-              isBasket={true}
-              isAvatar={true}
-              isName={false}
-              isBottom={false}
-          />
-        </div>
+    const [productsData,setProductsData] = useState([])
+    const [restaurantsData,setRestaurantsData] = useState([])
+        
+    const renderProducts = async () => {
+        const response = await getProducts()
+        setProductsData(response?.data.result.data);
+    }
 
-        <main className="p-[30px] flex justify-center">
-            <div className='max-w-[1440px] w-full flex gap-[40px]'>
-              <div className="left max-w-[251px] w-full py-[40px] pl-[25px] pr-[25px] bg-[#F3F4F6] flex flex-col gap-[25px]">
-                  <div className="filterItem flex gap-[17px] cursor-pointer">
-                      <Image
-                          src="/client/minifood/pizza.svg"
-                          alt="pizza"
-                          width={25}
-                          height={28}
-                      />
-                      <p className="text-[#333] text-[20px] font-semibold">
-                        Chinese
-                      </p>
-                  </div>
+    useEffect(() => {
+        renderProducts()
+    },[productsData])
 
-                  <div className="filterItem flex gap-[17px] cursor-pointer">
-                      <Image
-                          src="/client/minifood/pizza.svg"
-                          alt="pizza"
-                          width={25}
-                          height={28}
-                      />
-                      <p className="text-[#333] text-[20px] font-semibold">
-                        Chinese
-                      </p>
-                  </div>
 
-                  <div className="filterItem flex gap-[17px] cursor-pointer">
-                      <Image
-                          src="/client/minifood/pizza.svg"
-                          alt="pizza"
-                          width={25}
-                          height={28}
-                      />
-                      <p className="text-[#333] text-[20px] font-semibold">
-                        Chinese
-                      </p>
-                  </div>
-                  
-                  <div className="filterItem flex gap-[17px] cursor-pointer">
-                      <Image
-                          src="/client/minifood/pizza.svg"
-                          alt="pizza"
-                          width={25}
-                          height={28}
-                      />
-                      <p className="text-[#333] text-[20px] font-semibold">
-                        Chinese
-                      </p>
-                  </div>
-              </div>
+    const renderRestaurant = async () => {
+        const response = await getProducts()
+        setProductsData(response?.data.result.data);
+    }
 
-              <div className="cardBody grid grid-cols-4 gap-[40px]">
-                  <RestaurantCard
-                      path={"/client/restaurant/food.svg"}
-                      name={"Coffee Mania"}
-                      description={"chinese, sea-food, thai, lebanese, caribbean"}
-                      price={5}
-                      time={"09 Min"}
-                  />  
-                  <RestaurantCard
-                      path={"/client/restaurant/food.svg"}
-                      name={"Coffee Mania"}
-                      description={"chinese, sea-food, thai, lebanese, caribbean"}
-                      price={5}
-                      time={"09 Min"}
-                  />  
-                  <RestaurantCard
-                      path={"/client/restaurant/food.svg"}
-                      name={"Coffee Mania"}
-                      description={"chinese, sea-food, thai, lebanese, caribbean"}
-                      price={5}
-                      time={"09 Min"}
-                  />  
-                  <RestaurantCard
-                      path={"/client/restaurant/food.svg"}
-                      name={"Coffee Mania"}
-                      description={"chinese, sea-food, thai, lebanese, caribbean"}
-                      price={5}
-                      time={"09 Min"}
-                  />  
-                  <RestaurantCard
-                      path={"/client/restaurant/food.svg"}
-                      name={"Coffee Mania"}
-                      description={"chinese, sea-food, thai, lebanese, caribbean"}
-                      price={5}
-                      time={"09 Min"}
-                  />  
-              </div>
+    useEffect(() => {
+        renderRestaurant()
+    },[restaurantsData])
+
+    
+
+    return (
+        <div className="bg-white">
+            <div className="p-[30px]">
+            <Header
+                isLogin={true}
+                isBasket={true}
+                isAvatar={true}
+                isName={false}
+                isBottom={false}
+            />
             </div>
-        </main>
-       
 
-        <Footer
-            isTop={false}
-        />
-    </div>
-  );
+            <main className="p-[30px] flex justify-center">
+                <div className='max-w-[1440px] w-full flex gap-[40px]'>
+                <div className="left max-w-[251px] w-full py-[40px] pl-[25px] pr-[25px] bg-[#F3F4F6] flex flex-col gap-[25px]">
+                    <div className="filterItem flex gap-[17px] cursor-pointer">
+                        <Image
+                            src="/client/minifood/pizza.svg"
+                            alt="pizza"
+                            width={25}
+                            height={28}
+                        />
+                        <p className="text-[#333] text-[20px] font-semibold">
+                            Chinese
+                        </p>
+                    </div>
+
+                    <div className="filterItem flex gap-[17px] cursor-pointer">
+                        <Image
+                            src="/client/minifood/pizza.svg"
+                            alt="pizza"
+                            width={25}
+                            height={28}
+                        />
+                        <p className="text-[#333] text-[20px] font-semibold">
+                            Chinese
+                        </p>
+                    </div>
+
+                    <div className="filterItem flex gap-[17px] cursor-pointer">
+                        <Image
+                            src="/client/minifood/pizza.svg"
+                            alt="pizza"
+                            width={25}
+                            height={28}
+                        />
+                        <p className="text-[#333] text-[20px] font-semibold">
+                            Chinese
+                        </p>
+                    </div>
+                    
+                    <div className="filterItem flex gap-[17px] cursor-pointer">
+                        <Image
+                            src="/client/minifood/pizza.svg"
+                            alt="pizza"
+                            width={25}
+                            height={28}
+                        />
+                        <p className="text-[#333] text-[20px] font-semibold">
+                            Chinese
+                        </p>
+                    </div>
+                </div>
+
+                <div className="cardBody grid grid-cols-4 gap-[40px]">
+                    {
+                        productsData.map((item) => (
+                            <RestaurantCard
+                                detail={item}
+                            />  
+                        ))
+                    }
+                    
+                </div>
+                </div>
+            </main>
+        
+
+            <Footer
+                isTop={false}
+            />
+        </div>
+    );
 };
 
 export default Restaurants;
